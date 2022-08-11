8f84ef859b835b78ad821a23d200acb1cb933b30
omkarpai <1998opai@gmail.com>
2022-08-12 00:10:59 +0530

    add new game creation logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import ENV from "./env";
 import express from "express";
 import { indexRouter } from "./routes/IndexRouter";
 import path from "path";
+import { Server } from "socket.io";
+import BlackjackGamestate from "./lib/BlackjackGamestate";
+
 const app = express();
 
 app.use(express.static(path.resolve(__dirname + "../../client/build")));
@@ -11,3 +14,19 @@ app.use("*", indexRouter);
 app.listen(ENV.PORT);
 
 console.log("Listening on ", ENV.PORT);
+
+// Room ID is same as gameId
+const rooms: Map<string, BlackjackGamestate> = new Map();
+
+// Socket server
+const io = new Server(3001);
+io.on("connection", (socket) => {
+    socket.on("new-game", (msg) => {
+        const gameState = BlackjackGamestate.create();
+        rooms.set(gameState.getId(), gameState);
+        socket.join(gameState.getId());
+        socket.emit("game-create-success", gameState);
+    });
+
+    socket.on("join-game", (msg) => {});
+});
