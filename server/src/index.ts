920fe8f1e9d34884d97a4b04509e786bc99495ad
omkarpai <1998opai@gmail.com>
2022-08-13 00:12:47 +0530

    add connection handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,9 @@ import ENV from "./env";
 import express from "express";
 import { indexRouter } from "./routes/IndexRouter";
 import path from "path";
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 import BlackjackGamestate from "./lib/BlackjackGamestate";
+import log from "./logger/Logger";
 
 const app = express();
 
@@ -13,20 +14,57 @@ app.use("*", indexRouter);
 
 app.listen(ENV.PORT);
 
-console.log("Listening on ", ENV.PORT);
+log(`Listening on ${ENV.PORT}`);
 
+// ________________ GAMESTATES ________________
 // Room ID is same as gameId
 const rooms: Map<string, BlackjackGamestate> = new Map();
+
+// ________________ SOCKETID to GAMEID MAP ______________________
+const socketIdToGameIdMap: Map<string, string> = new Map();
+
+const handleGameError = (e: any, socket: Socket) => {
+    const message = e.message;
+    log(message);
+    socket.emit("game-error", new GameError({ message }));
+};
+
+const getGamestate = (gameId: string): BlackjackGamestate => {
+    const gameState = rooms.get(gameId);
+    if (!gameState) throw Error(`Unable to find gameState for ID ${gameId}`);
+    return gameState;
+};
 
 // Socket server
 const io = new Server(3001);
 io.on("connection", (socket) => {
-    socket.on("new-game", (msg) => {
+    socket.on("new-game", () => {
         const gameState = BlackjackGamestate.create();
         rooms.set(gameState.getId(), gameState);
         socket.join(gameState.getId());
-        socket.emit("game-create-success", gameState);
+        socket.emit("token", { gameId: gameState.getId(), token: gameState.getP1Token() });
     });
 
-    socket.on("join-game", (msg) => {});
+    socket.on("join-game", (msg: JoinGameMessage) => {
+        try {
+            const g = getGamestate(msg.gameId);
+            if (!msg.token) {
+                socket.emit("token", { gameId: g.getId(), token: g.getP2Token() });
+                return;
+            }
+            g.handleJoinGame(msg);
+            socket.join(g.getId());
+        } catch (e: any) {
+            handleGameError(e, socket);
+        }
+    });
+
+    socket.on("received-token", (msg: ReceivedTokenMessage) => {
+        try {
+            const g = getGamestate(msg.gameId);
+            g.setAssignedToken(msg);
+        } catch (e: any) {
+            handleGameError(e, socket);
+        }
+    });
 });
