614370fc184b24bf3c85a355fb6c039391ea0a33
omkarpai <1998opai@gmail.com>
2022-08-13 20:30:43 +0530

    refactor tokens n connection data to own class

--- server/src/lib/BlackjackGamestate.ts
+++ server/src/lib/BlackjackGamestate.ts
@@ -1,4 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
+import PlayerConnectionData from "./PlayerConnectionData";
+import PlayerGameData from "./PlayerGameData";
 
 interface BlackjackGamestateConstructor {
     id: string;
@@ -7,30 +9,52 @@ interface BlackjackGamestateConstructor {
 }
 
 class BlackjackGamestate {
-    handleJoinGame(msg: JoinGameMessage) {
-        this.validateGameId(msg.gameId);
-        this.validateTokenBelongsToThisGamestate(msg);
+    public handleJoinGame(token: string, socketId: string) {
+        this.validateTokenBelongsToThisGamestate(token);
+        const pcd = this.getPlayerConnectionDataByToken(token);
+        pcd.handleSocketConnect(socketId);
     }
 
-    private validateTokenBelongsToThisGamestate(msg: JoinGameMessage) {
-        if (msg.token === this.p1Token) return;
-        if (msg.token === this.p2Token) return;
-        throw Error(`token in msg ${msg.token} was not assigned in this game`);
+    public handleLeaveGame(socketId: string) {
+        const pcd = this.getPlayerConnectionDataBySocketId(socketId);
+        pcd.handleSocketDisconnect();
     }
+
+    private validateTokenBelongsToThisGamestate(token: string) {
+        if (token === this.p1ConnectionData.getToken()) return;
+        if (token === this.p2ConnectionData.getToken()) return;
+        throw Error(`token in msg ${token} was not assigned in this game`);
+    }
+
+    private getPlayerGameDataByToken = (token: string) => {
+        if (token === this.p1ConnectionData.getToken()) return this.p1GameData;
+        if (token === this.p2ConnectionData.getToken()) return this.p2GameData;
+        throw new Error(`Unable to get player game data for ${token}`);
+    };
+
+    private getPlayerConnectionDataByToken = (token: string) => {
+        if (token === this.p1ConnectionData.getToken()) return this.p1ConnectionData;
+        if (token === this.p2ConnectionData.getToken()) return this.p2ConnectionData;
+        throw new Error(`Unable to get player connection data for ${token}`);
+    };
+
+    private getPlayerConnectionDataBySocketId = (socketId: string) => {
+        if (socketId === this.p1ConnectionData.getSocketId()) return this.p1ConnectionData;
+        if (socketId === this.p2ConnectionData.getSocketId()) return this.p2ConnectionData;
+        throw new Error(`Unable to get player connection data for socket${socketId}`);
+    };
 
     private id: string;
 
-    private p1Token: string;
-    private isP1TokenAssigned: boolean;
-    private p2Token: string;
-    private isP2TokenAssigned: boolean;
+    private p1ConnectionData: PlayerConnectionData;
+    private p2ConnectionData: PlayerConnectionData;
+    private p1GameData: PlayerGameData;
+    private p2GameData: PlayerGameData;
 
     private constructor(obj: BlackjackGamestateConstructor) {
         this.id = obj.id;
-        this.p1Token = obj.p1Token;
-        this.isP1TokenAssigned = false;
-        this.p2Token = obj.p2Token;
-        this.isP2TokenAssigned = false;
+        this.p1ConnectionData = new PlayerConnectionData(obj.p1Token);
+        this.p2ConnectionData = new PlayerConnectionData(obj.p2Token);
     }
 
     public static create(): BlackjackGamestate {
@@ -45,17 +69,17 @@ class BlackjackGamestate {
     // Example: if playerToken belongs to p1, p2 details are sanitised
     private getSanitisedCopyToSendTo(playerToken: string): BlackjackGamestate {
         const copy: BlackjackGamestate = { ...this };
-        if (playerToken === this.p1Token) copy.sanitisePlayer2();
-        if (playerToken === this.p2Token) copy.sanitisePlayer1();
+        if (playerToken === this.p1ConnectionData.getToken()) copy.sanitisePlayer2();
+        if (playerToken === this.p2ConnectionData.getToken()) copy.sanitisePlayer1();
         return copy;
     }
 
     private sanitisePlayer1(): void {
-        this.p1Token = "";
+        this.p1ConnectionData.sanitise();
     }
 
     private sanitisePlayer2(): void {
-        this.p2Token = "";
+        this.p2ConnectionData.sanitise();
     }
 
     public getId() {
@@ -63,27 +87,24 @@ class BlackjackGamestate {
     }
 
     public getP1Token() {
-        return this.p1Token;
+        return this.p1ConnectionData.getToken();
     }
 
     public getP2Token() {
-        return this.p2Token;
+        return this.p2ConnectionData.getToken();
     }
 
-    private validateGameId = (gid: string) => {
-        if (gid !== this.id) throw Error(`GID error this:${this.id} token:${gid}`);
+    private validateAtleastSingleTokenAssigned = () => {
+        const p1TokenAssigned = this.p1ConnectionData.getIsTokenAssigned();
+        const p2TokenAssigned = this.p2ConnectionData.getIsTokenAssigned();
+        if (p1TokenAssigned || p2TokenAssigned) return;
+        throw Error("Unable to assign any token");
     };
 
     public setAssignedToken(msg: ReceivedTokenMessage) {
-        this.validateGameId(msg.gameId);
-        if (msg.token === this.p1Token) {
-            if (this.isP1TokenAssigned) throw Error(`p1 token is already asssigned`);
-            this.isP1TokenAssigned = true;
-        }
-        if (msg.token === this.p2Token) {
-            if (this.isP2TokenAssigned) throw Error(`p2 token is already asssigned`);
-            this.isP2TokenAssigned = true;
-        }
+        this.p1ConnectionData.assignToken(msg);
+        this.p2ConnectionData.assignToken(msg);
+        this.validateAtleastSingleTokenAssigned();
     }
 }
 
