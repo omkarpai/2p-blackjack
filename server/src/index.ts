ceb69dfd9a910d781113151d3ddbc65c34df3d8c
omkarpai <1998opai@gmail.com>
2022-08-13 20:30:09 +0530

    add disconnect handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,13 +26,23 @@ const socketIdToGameIdMap: Map<string, string> = new Map();
 const handleGameError = (e: any, socket: Socket) => {
     const message = e.message;
     log(message);
-    socket.emit("game-error", new GameError({ message }));
+    try {
+        socket.emit("game-error", new GameError({ message }));
+    } catch (e: any) {
+        log(e.message);
+    }
 };
 
-const getGamestate = (gameId: string): BlackjackGamestate => {
+const getGamestateByGameId = (gameId: string): BlackjackGamestate => {
     const gameState = rooms.get(gameId);
     if (!gameState) throw Error(`Unable to find gameState for ID ${gameId}`);
     return gameState;
+};
+
+const getGamestateBySocketId = (socketId: string): BlackjackGamestate => {
+    const gameId = socketIdToGameIdMap.get(socketId);
+    if (!gameId) throw Error(`Unable to find gameId for socketId ${socketId}`);
+    return getGamestateByGameId(gameId);
 };
 
 // Socket server
@@ -41,19 +51,29 @@ io.on("connection", (socket) => {
     socket.on("new-game", () => {
         const gameState = BlackjackGamestate.create();
         rooms.set(gameState.getId(), gameState);
-        socket.join(gameState.getId());
         socket.emit("token", { gameId: gameState.getId(), token: gameState.getP1Token() });
     });
 
     socket.on("join-game", (msg: JoinGameMessage) => {
         try {
-            const g = getGamestate(msg.gameId);
+            const g = getGamestateByGameId(msg.gameId);
             if (!msg.token) {
                 socket.emit("token", { gameId: g.getId(), token: g.getP2Token() });
                 return;
             }
-            g.handleJoinGame(msg);
+            g.handleJoinGame(msg.gameId, msg.token);
+            socketIdToGameIdMap.set(socket.id, g.getId());
             socket.join(g.getId());
+        } catch (e: any) {
+            handleGameError(e, socket);
+        }
+    });
+
+    socket.on("disconnect", () => {
+        try {
+            const socketId = socket.id;
+            const gameState = getGamestateBySocketId(socketId);
+            gameState.handleLeaveGame(socketId);
         } catch (e: any) {
             handleGameError(e, socket);
         }
@@ -61,7 +81,7 @@ io.on("connection", (socket) => {
 
     socket.on("received-token", (msg: ReceivedTokenMessage) => {
         try {
-            const g = getGamestate(msg.gameId);
+            const g = getGamestateByGameId(msg.gameId);
             g.setAssignedToken(msg);
         } catch (e: any) {
             handleGameError(e, socket);
