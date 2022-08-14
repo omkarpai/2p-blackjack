8ecbdc177e255baf02f7d5b25c06ba83e4a095dd
omkarpai <1998opai@gmail.com>
2022-08-14 22:08:53 +0530

    add method to get top card and shuffle

--- server/src/lib/Deck.ts
+++ server/src/lib/Deck.ts
@@ -6,6 +6,7 @@ class Deck {
     private cards: Array<Card>;
     constructor() {
         this.cards = Deck.generate();
+        this.shuffle();
     }
 
     private static getCardValueEntries() {
@@ -26,6 +27,35 @@ class Deck {
             }
         }
         return cards;
+    }
+
+    // Shuffle implementation reference
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    private shuffle() {
+        const array = this.cards;
+        let currentIndex = array.length;
+        let randomIndex;
+
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+            // Pick a remaining element.
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            // And swap it with the current element.
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+    }
+
+    public resetAndShuffle() {
+        this.cards = Deck.generate();
+        this.shuffle();
+    }
+
+    public getTopCard(): Card {
+        const topCard = this.cards.pop();
+        if (!topCard) throw Error("Cannot get top card as deck is empty");
+        return topCard;
     }
 }
 
