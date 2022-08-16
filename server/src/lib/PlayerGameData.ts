ab0af2c3ce13823372270e400665953a46b27ab8
omkarpai <1998opai@gmail.com>
2022-08-17 00:05:51 +0530

    add validations for current turn status

--- server/src/lib/PlayerGameData.ts
+++ server/src/lib/PlayerGameData.ts
@@ -1,4 +1,5 @@
 import Card from "./Card";
+import RoundStatus from "./RoundStatus";
 import TurnStatus from "./TurnStatus";
 
 interface PlayerGameDataConstructor {
@@ -11,21 +12,33 @@ class PlayerGameData {
     private isBust: boolean;
     private turnStatus: TurnStatus;
     private total: number;
+    public roundStatus: RoundStatus | undefined;
     constructor(obj: PlayerGameDataConstructor) {
         this.upCard = obj.upCard;
         this.downCards = new Array<Card>();
         this.isBust = false;
         this.turnStatus = TurnStatus.WAITING;
         this.total = obj.upCard.value;
+        this.roundStatus = undefined;
     }
 
     public endTurn() {
-        this.turnStatus = TurnStatus.DONE;
+        this.validateTurnNotOver();
+        this.turnStatus = TurnStatus.OVER;
     }
 
     public addToDownCards(card: Card) {
+        this.validateTurnNotOver();
         this.downCards.push(card);
         this.calculateTotalAndDetermineIfBust();
+    }
+
+    public getTurnStatus() {
+        return this.turnStatus;
+    }
+
+    private validateTurnNotOver() {
+        if (this.turnStatus === TurnStatus.OVER) throw Error(`Unable to make move.status is DONE`);
     }
 
     private calculateTotalAndDetermineIfBust() {
@@ -35,6 +48,7 @@ class PlayerGameData {
 
     private determineIsBust() {
         this.isBust = this.total > 21 ? true : false;
+        if (this.isBust) this.turnStatus = TurnStatus.OVER;
     }
 
     private calculateTotal() {
